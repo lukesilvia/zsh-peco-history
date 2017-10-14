5e81b474a193220b6a3332b22f0c57151228b734
lukesilvia <lukesilvia@gmail.com>
2017-10-14 16:46:05 +0900

    fit to work on my machine

--- zsh-peco-history.zsh
+++ zsh-peco-history.zsh
@@ -7,12 +7,12 @@
 #
 if which peco &> /dev/null; then
   function peco_select_history() {
-    local tac
-    ((($+commands[gtac])) && tac="gtac") || \
-      (($+commands[tac])) && tac="tac" || \
-      tac="tail -r"
-    BUFFER=$(fc -l -n 1 | eval $tac | \
-               peco --layout=bottom-up --query "$LBUFFER")
+    if which tac >/dev/null; then
+      BUFFER=`history -n 1 | tac | awk '!a[$0]++' | peco`
+    else
+      BUFFER=`history -n 1 | tail -r | awk '!a[$0]++' | peco`
+    fi
+
     CURSOR=$#BUFFER # move cursor
     zle -R -c       # refresh
   }
